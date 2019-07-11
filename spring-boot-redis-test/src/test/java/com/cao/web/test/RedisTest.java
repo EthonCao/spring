10ef3bccbe00d92cff258b29cb435997aaf8b950
Cao <Cao@DESKTOP-L93V3IC>
2019-07-11 23:44:00 +0800

    SpringBoot integrate with spring data redis and demo

---
+++ spring-boot-redis-test/src/test/java/com/cao/web/test/RedisTest.java
@@ -0,0 +1,41 @@
+package com.cao.web.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cao.web.App;
+
+
+/**
+ * Spring Data Redis测试
+ *
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=App.class)
+public class RedisTest {
+
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+	
+	/**
+	 * 添加一个字符串
+	 */
+	@Test
+	public void testSet(){
+		this.redisTemplate.opsForValue().set("caoTestKey", "This is my first Redis value");
+	}
+	
+	/**
+	 * 获取一个字符串
+	 */
+	@Test
+	public void testGet(){
+		String value = (String)this.redisTemplate.opsForValue().get("caoTestKey");
+		System.out.println(value);
+	}
+}
